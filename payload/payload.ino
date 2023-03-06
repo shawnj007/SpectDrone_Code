177ed5113a16cde9690fe63fd5e7b3182f656b75
Shawn <shawn.jaker@gmail.com>
2023-03-06 15:53:34 -0700

    SpectDrone Development

--- payload/payload.ino
+++ payload/payload.ino
@@ -6,48 +6,70 @@
 #include "pin_assignments.h"
 
 #include <SoftwareSerial.h>
-SoftwareSerial* radio;
+SoftwareSerial* radio = NULL;
+SoftwareSerial* gps_ss = NULL;
+SoftwareSerial* camera = NULL;
+
+#include <TinyGPS++.h>
+TinyGPSPlus* gps = NULL;
+
+#include <Adafruit_BNO055.h>
+Adafruit_BNO055* orien = NULL;
+
+#include <Adafruit_BME280.h>
+Adafruit_BME280* thp = NULL;
+
+#include <Adafruit_MLX90614.h>
+Adafruit_MLX90614* ir = NULL;
 
 int setup_radio(int rx, int tx) {
 	// Setup Radio pins
 	// Returns 0 on sucess, 1 on failure.
 	radio = new SoftwareSerial(rx, tx);
-	if (radio != NULL) return 0; else return 1;
+	if (radio == NULL) return 1;
+	else return 0;
 }
 
 int setup_gps(int rx, int tx) {
 	// Setup Global Positioning System pins
 	// Returns 0 on sucess, 1 on failure.
-
-	if (gps != NULL) return 0; else return 1;
+	gps_ss = new SoftwareSerial(rx, tx);
+	gps = new TinyGPSPlus();
+	if (gps_ss == NULL || gps == NULL) return 1;
+	else return 0;
 }
 
 int setup_camera(int rx, int tx) {
 	// Setup Camera pins
 	// Returns 0 on sucess, 1 on failure.
-
-	return 1;
+	camera = new SoftwareSerial(rx, tx);
+	if (camera == NULL) return 1;
+	else return 0;
 }
 
 int setup_imu(int sda, int scl) {
-	// Setup Camera pins
+	// Setup Orientation pins
 	// Returns 0 on sucess, 1 on failure.
-
-	return 1;
+	orien = new Adafruit_BNO055(55);
+	if (orien == NULL || !orien->begin()) return 1;
+	else return 0;
 }
 
 int setup_thp(int sda, int scl) {
 	// Setup Temperature, Humidity, Pressure Sensor pins
 	// Returns 0 on sucess, 1 on failure.
-
+	thp = new Adafruit_BME280();
+	if (thp == NULL || !thp->begin()) return 1;
+	else return 0;
 	return 1;
 }
 
 int setup_infrared(int sda, int scl) {
 	// Setup Infrared Thermal Sensor pins
 	// Returns 0 on sucess, 1 on failure.
-
-	return 1;
+	ir = new Adafruit_MLX90614();
+	if (ir == NULL || !ir->begin()) return 1;
+	return 0;
 }
 
 int setup_spect(int clock, int st, int gain, int video) {
@@ -66,31 +88,36 @@ void fail_on(char* failed) {
 }
 
 void setup() {
-	// Set up RADIO
 	if (setup_radio(RADIO_RX, RADIO_TX) != 0) fail_on("RADIO");
-
-	// Set up GPS
 	if (setup_gps(GPS_RX, GPS_TX) != 0) fail_on("GPS");
-
-	// Set up CAMERA
 	if (setup_camera(CAM_RX, CAM_TX) != 0) fail_on("CAMERA");
-
-	// Set up ORIENTATION
 	if (setup_imu(IMU_SDA, IMU_SCL) != 0) fail_on("ORIENTATION");
-
-	// Set up TEMPERATURE, HUMIDITY, PRESSURE SENSOR
 	if (setup_thp(THP_SDA, THP_SCL) != 0) fail_on("TEMPERATURE, HUMIDITY, PRESSURE SENSOR");
-
-	// Set up IR
 	if (setup_infrared(IR_SDA, IR_SCL) != 0) fail_on("INFRARED");
-
-	// Set up Spectroradiometer 1
 	if (setup_spect(SPECT1_CLK, SPECT1_ST, SPECT1_GAIN, SPECT1_VIDEO) != 0) fail_on("SPECTORADIOMETER 1");
-
-	// Set up Spectroradiometer 2
 	if (setup_spect(SPECT2_CLK, SPECT2_ST, SPECT2_GAIN, SPECT2_VIDEO) != 0) fail_on("SPECTORADIOMETER 2");
+}
+
+void read_commands() {
+	// Read commands from base station
+}
+
+void exec_commands() {
+	// Execute commands from base station
+}
+
+void read_telemetry() {
+	// Read sensor data and telemetry
+}
+
+void send_telemetry() {
+	// Send sensor data and telemetry
 }
 
 void loop() {
 	// put your main code here, to run repeatedly:
+	read_commands();
+	exec_commands();
+	read_telemetry();
+	send_telemetry();
 }
